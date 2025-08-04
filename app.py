fea4950db42258281b8d415ec26317e64737fc0b
Deepika Gupta <161952219+Deepikasinghal91@users.noreply.github.com>
2025-08-04 17:27:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pickle
+movies_list=pickle.load(open('movies.pkl','rb'))
+movies_list=movies_list['title'].values
+st.title('Movie Recommender System')
+option =st.selectbox(
+movies_list
+)
